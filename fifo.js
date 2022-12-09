941b4688e7801e038391aee5933784aa3451afb1
klebaniukp <klebaniukp@gmail.com>
2022-12-10 00:32:09 +0100

    push_head method, synchronizing local fields with storage

--- fifo.js
+++ fifo.js
@@ -9,11 +9,21 @@ export class FifoQueue {
         if (!(await this.isQueueAlreadyCreated())) {
             localforage.setItem(this.queueName, {});
         }
+        await this.setHeadAndTail();
+    }
+
+    async setHeadAndTail() {
         this.head = await this.getValueFromStorage('Head');
         this.tail = await this.getValueFromStorage('Tail');
     }
 
-    async getValueFromStorage(keyElement) {
+    async setKeyInStorage(keyElement = '', value) {
+        const fullKey = this.queueName + keyElement;
+
+        await localforage.setItem(fullKey, value);
+    }
+
+    async getValueFromStorage(keyElement = '') {
         const fullKey = this.queueName + keyElement;
         const response = await localforage.getItem(fullKey);
 
@@ -28,26 +38,45 @@ export class FifoQueue {
         return (await localforage.getItem(this.queueName)) != null;
     }
 
-    // async push_head(value) {
-    //     const coreName = this.queueName + '-' + (await this.generateId());
-    //     const prev = '';
-    //     // const  = await localforage.getItem(this.queueName);
-    //     //edit current head to make his prev not empty
-    // }
+    async push_head(value = '') {
+        const id = await this.generateId();
+
+        await this.setKeyInStorage(`Element-${id}-Next`, this.head);
+        await this.setKeyInStorage(`Element-${id}-Prev`, '');
+        await this.setKeyInStorage(`Element-${id}-Value`, value);
+
+        await this.setKeyInStorage(
+            `Element-${this.head}-Prev`,
+            `${this.queueName}Element-${id}`,
+        );
+
+        if (this.tail === {}) {
+            await this.setKeyInStorage('Tail', this.queueName + id);
+            await this.setHeadAndTail();
+        }
+
+        await this.setKeyInStorage('Head', this.queueName + id);
+    }
 
     async getElements() {
         return await localforage.getItem(this.queueName);
     }
 
     async generateId() {
-        const lastIndexKeyName = this.queueName + this.tail;
+        const lastIndexKeyName = this.queueName + 'LastIndex';
 
         const lastIndex = await localforage.getItem(lastIndexKeyName);
         if (lastIndex != null) {
-            return lastIndex;
+            await this.setNewLastIndex(lastIndex);
+            return lastIndex + 1;
         } else {
+            await this.setNewLastIndex();
             return 1;
         }
+    }
+
+    async setNewLastIndex(lastIndex = 1) {
+        await localforage.setItem(this.queueName + 'LastIndex', lastIndex + 1);
     }
 
     async remove() {
